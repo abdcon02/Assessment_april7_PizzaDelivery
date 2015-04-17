2dd897957be1c81cea4f1f96e84cff3b9ac6407e
Connor <connor.abd@gmail.com>
2015-04-17 09:43:57 -0700

    Add tests and passing methods for calculating pizza cost depending on properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert("hello");
 var MakePizza = {
-    size: "Large"
-}
+    size: "Large",
+    topping: "Plain",
+    calculateCost: function() {
+        var cost = 0;
+        if (this.size === "Large"){
+            cost += 13;
+        } else if (this.size === "Medium") {
+            cost += 11;
+        } else {
+            cost += 9;
+        }
+        if (this.topping === "Cheese") {
+            cost += 2;
+        } else if (this.topping === "Pepperoni") {
+            cost += 3;
+        }
+        return cost;
+    }
+};
--- specs/specs.js
+++ specs/specs.js
@@ -1,10 +1,35 @@
 describe("MakePizza", function() {
-
     it("Makes a pizza with a size of Large", function() {
         var testPizza = Object.create(MakePizza);
         expect(testPizza.size).to.equal("Large");
     });
-    // describe("calculateCost", function() {
-    //
-    // })
+    it("Makes a pizza with a topping of cheese", function() {
+        var testPizza = Object.create(MakePizza);
+        testPizza.topping = "Cheese";
+        expect(testPizza.topping).to.equal("Cheese");
+    });
+    describe("calculateCost", function() {
+        it("Takes a pizza with a topping of Cheese and size of Large and calculates a price of $15", function() {
+            var testPizza = Object.create(MakePizza);
+            testPizza.topping = "Cheese";
+            expect(testPizza.calculateCost()).to.equal(15);
+        });
+        it("Takes a pizza with a topping of Pepperoini and size of Large and calculates a price of $16", function() {
+            var testPizza = Object.create(MakePizza);
+            testPizza.topping = "Pepperoni";
+            expect(testPizza.calculateCost()).to.equal(16);
+        });
+        it("Takes a pizza with a topping of Pepperoini and size of Medium and calculates a price of $14", function() {
+            var testPizza = Object.create(MakePizza);
+            testPizza.size = "Medium";
+            testPizza.topping = "Pepperoni";
+            expect(testPizza.calculateCost()).to.equal(14);
+        });
+        it("Takes a pizza with a topping of Pepperoini and size of Small and calculates a price of $12", function() {
+            var testPizza = Object.create(MakePizza);
+            testPizza.size = "Small";
+            testPizza.topping = "Pepperoni";
+            expect(testPizza.calculateCost()).to.equal(12);
+        });
+    });
 });
